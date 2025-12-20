381b0dfab7d457aa5ddeeaf09add2d850b065eca
Xuechun (Alexa) Sun <xuechunsun@gmail.com>
2025-12-20 11:58:32 -0800

    Initial Commit

---
+++ LifeExperiment/LifeExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeExperimentApp.swift
+//  LifeExperiment
+//
+//  Created by Xuechun Sun on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
